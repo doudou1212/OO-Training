6131ba27e6e044e4995f06de00705429992cc49b
Jing Sun <jing.sun@rea-group.com>
2019-04-04 08:48:34 +0800

    add constructor for parking lot

--- src/main/homework/ParkingLot.java
+++ src/main/homework/ParkingLot.java
@@ -1,6 +1,12 @@
 package main.homework;
 
 public class ParkingLot {
+    private int capacity;
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
+
     public Ticket parking(Car car) {
         return new Ticket();
     }

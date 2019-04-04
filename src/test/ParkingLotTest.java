45326cfde550e481cc16e93b7cf80715b3e05756
Jing Sun <jing.sun@rea-group.com>
2019-04-04 08:51:04 +0800

    Add test for scenario: parking to a full parking lot

--- src/test/ParkingLotTest.java
+++ src/test/ParkingLotTest.java
@@ -6,6 +6,7 @@ import main.homework.Ticket;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class ParkingLotTest {
     @Test
@@ -14,5 +15,13 @@ public class ParkingLotTest {
         Car car = new Car();
         Ticket ticket = parkingLot.parking(car);
         assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_not_get_ticket_when_parking_given_an_full_parking_lot() {
+        ParkingLot parkingLot = new ParkingLot(1);
+        parkingLot.parking(new Car());
+        Car myCar = new Car();
+        assertNull(parkingLot.parking(myCar));
     }
 }

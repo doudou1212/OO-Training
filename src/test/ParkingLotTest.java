a8c94422c15f3ead0d2422c53a34ae9ce22a02ae
Jing Sun <jing.sun@rea-group.com>
2019-04-04 08:47:32 +0800

    should make sense for 'available parking lot' in test case

--- src/test/ParkingLotTest.java
+++ src/test/ParkingLotTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertNotNull;
 public class ParkingLotTest {
     @Test
     public void should_get_ticket_when_parking_given_an_available_parking_lot() {
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(1);
         Car car = new Car();
         Ticket ticket = parkingLot.parking(car);
         assertNotNull(ticket);

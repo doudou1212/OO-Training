c15babb209ef09a5140433eca9c2b765a71fc2ac
Jing Sun <jing.sun@rea-group.com>
2019-04-04 09:01:06 +0800

    Add implemetation for a full parking lot when parking

--- src/main/homework/ParkingLot.java
+++ src/main/homework/ParkingLot.java
@@ -1,13 +1,22 @@
 package main.homework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
     private int capacity;
+    private Map<Ticket, Car> parkingLots = new HashMap<>();
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
     }
 
     public Ticket parking(Car car) {
-        return new Ticket();
+        if(parkingLots.size() < capacity) {
+            Ticket ticket = new Ticket();
+            parkingLots.put(ticket, car);
+            return ticket;
+        }
+        return null;
     }
 }

6bc36448b57c0e3059439b334651a0036de0c39f
Jing Sun <jing.sun@rea-group.com>
2019-04-06 20:57:06 +0800

    Add implemetation for parking to the lot which has most spaces

---
+++ src/main/homework/SmartParkingBoy.java
@@ -0,0 +1,15 @@
+package main.homework;
+
+import java.util.List;
+
+public class SmartParkingBoy {
+    private List<ParkingLot> parkingLots;
+
+    public SmartParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+    }
+
+    public Ticket parking(Car car) {
+        return parkingLots.get(1).parking(car);
+    }
+}

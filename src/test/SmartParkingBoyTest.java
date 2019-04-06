4fa6cc25e1de0ca8fecae59935e900490e8347bd
Jing Sun <jing.sun@rea-group.com>
2019-04-06 20:55:02 +0800

    Add test case for smart parking boy: park car to the parking lot which has most spaces

---
+++ src/test/SmartParkingBoyTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import main.homework.ParkingLot;
+import main.homework.Ticket;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmartParkingBoyTest {
+    @Test
+    public void should_park_at_second_parking_lot_when_parking_give_smart_parking_boy_has_two_lots_with_1st_lot_has_one_space_2en_lot_has_two_spaces() {
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(2);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+        Ticket ticket = smartParkingBoy.parking(car);
+        assertSame(car, parkingLot2.pick(ticket));
+    }
+}

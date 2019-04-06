8b72161f031d780dc1bba5d5fb2da6efa5481f27
Jing Sun <jing.sun@rea-group.com>
2019-04-06 13:44:55 +0800

    add parking boy test: should get ticket when parking given parking lots has available space

---
+++ src/test/ParkingBoyTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import main.day4.Ticket;
+import main.homework.Car;
+import main.homework.ParkingLot;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ParkingBoyTest {
+    @Test
+    public void should_get_ticket_when_parking_given_parking_boy_has_one_parking_lot_with_one_space() {
+        ParkingLot parkingLot = new ParkingLot(1);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+        Ticket ticket = parkingBoy.parking(new Car());
+        assertNotNull(ticket);
+    }
+}

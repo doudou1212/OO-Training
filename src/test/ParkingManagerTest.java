651c5fc47c7895dfe90e10d253f4596a736b89ea
Jing Sun <jing.sun@rea-group.com>
2019-04-07 16:08:12 +0800

    Add test case for manager: manager should parking success when all parking boys' lots are not available and manager has available parking lto

--- src/test/ParkingManagerTest.java
+++ src/test/ParkingManagerTest.java
@@ -79,4 +79,36 @@ public class ParkingManagerTest {
         Ticket ticket = manager.parking(car);
         assertSame(car, manager.pick(ticket));
     }
+
+    @Test
+    public void should_get_parking_ticket_when_parking_given_parking_manager_has_two_boys_and_no_boy_has_one_available_parking_lot() {
+        ParkingLot boy1ParkingLot = new ParkingLot(1);
+        List<ParkingLot> boy1ParkingLots = new ArrayList<>();
+        boy1ParkingLots.add(boy1ParkingLot);
+        SmartParking smartParking = new SmartParking();
+        DefaultPick defaultPick = new DefaultPick();
+        ParkingBoy smartParkingBoy = new ParkingBoy(smartParking, defaultPick, boy1ParkingLots);
+        smartParkingBoy.parking(new Car());
+
+        ParkingLot boy2ParkingLot = new ParkingLot(1);
+        List<ParkingLot> boy2ParkingLots = new ArrayList<>();
+        boy2ParkingLots.add(boy2ParkingLot);
+        DefaultParking defaultParking = new DefaultParking();
+        ParkingBoy parkingBoy = new ParkingBoy(defaultParking, defaultPick, boy2ParkingLots);
+        parkingBoy.parking(new Car());
+
+        List<ParkingBoy> boys = new ArrayList<>();
+        boys.add(smartParkingBoy);
+        boys.add(parkingBoy);
+
+        ManagerParking managerParking = new ManagerParking();
+        ManagePick managePick = new ManagePick();
+        ParkingLot managerParkingLot = new ParkingLot(1);
+        List<ParkingLot> managerParkingLots = new ArrayList<>();
+        managerParkingLots.add(managerParkingLot);
+        ParkingManager manager = new ParkingManager(managerParking, managePick, boys, managerParkingLots);
+        Car car = new Car();
+        Ticket ticket = manager.parking(car);
+        assertSame(car, manager.pick(ticket));
+    }
 }

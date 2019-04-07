912c5d76af8ed2e4cab52b92234ec127fa35ddc5
Jing Sun <jing.sun@rea-group.com>
2019-04-07 16:17:14 +0800

    Add test case for manager: parking failed when no available parking lots

--- src/test/ParkingManagerTest.java
+++ src/test/ParkingManagerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 public class ParkingManagerTest {
@@ -136,5 +137,36 @@ public class ParkingManagerTest {
         Ticket ticket2 = manager.parking(car2);
         assertSame(car1, manager.pick(ticket1));
         assertSame(car2, manager.pick(ticket2));
+    }
+
+    @Test
+    public void should_parking_failed_when_parking_given_parking_manager_has_two_boys_and_no_boy_or_has_available_parking_lot() {
+        ParkingLot boy1ParkingLot = new ParkingLot(1);
+        List<ParkingLot> boy1ParkingLots = new ArrayList<>();
+        boy1ParkingLots.add(boy1ParkingLot);
+        SmartParking smartParking = new SmartParking();
+        DefaultPick defaultPick = new DefaultPick();
+        ParkingBoy smartParkingBoy = new ParkingBoy(smartParking, defaultPick, boy1ParkingLots);
+        smartParkingBoy.parking(new Car());
+
+        ParkingLot boy2ParkingLot = new ParkingLot(1);
+        List<ParkingLot> boy2ParkingLots = new ArrayList<>();
+        boy2ParkingLots.add(boy2ParkingLot);
+        DefaultParking defaultParking = new DefaultParking();
+        ParkingBoy parkingBoy = new ParkingBoy(defaultParking, defaultPick, boy2ParkingLots);
+        parkingBoy.parking(new Car());
+
+        List<ParkingBoy> boys = new ArrayList<>();
+        boys.add(smartParkingBoy);
+        boys.add(parkingBoy);
+
+        ManagerParking managerParking = new ManagerParking();
+        ManagePick managePick = new ManagePick();
+        ParkingLot managerParkingLot = new ParkingLot(1);
+        List<ParkingLot> managerParkingLots = new ArrayList<>();
+        managerParkingLots.add(managerParkingLot);
+        ParkingManager manager = new ParkingManager(managerParking, managePick, boys, managerParkingLots);
+        manager.parking(new Car());
+        assertNull(manager.parking(new Car()));
     }
 }

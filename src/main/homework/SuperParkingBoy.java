1313495c015e98a45a06556d661c1055efde9953
Jing Sun <jing.sun@rea-group.com>
2019-04-07 11:03:10 +0800

    perfact implementtation for super parking boy test case: should park car to the parking lots which has the most spaces when the lots have same reate

--- src/main/homework/SuperParkingBoy.java
+++ src/main/homework/SuperParkingBoy.java
@@ -2,6 +2,7 @@ package main.homework;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class SuperParkingBoy extends ParkingMan {
     public SuperParkingBoy(List<ParkingLot> parkingLots) {
@@ -11,10 +12,15 @@ public class SuperParkingBoy extends ParkingMan {
     @Override
     public Ticket parking(Car car) {
         final Ticket[] ticket = new Ticket[1];
+        double mostRate = 0.0;
+        Optional<ParkingLot> targetParkingLot = getParkingLots().stream()
+                .max(Comparator.comparing(parkingLot -> parkingLot.getParkingRate()));
+        mostRate = targetParkingLot.orElse(null).getParkingRate();
+
+        double finalMostRate = mostRate;
         getParkingLots().stream()
-                .filter(parkingLot -> parkingLot.getAvailbaleSpaces() > 0)
-                .sorted(Comparator.comparing(parkingLot -> parkingLot.getParkingRate()))
-                .findFirst()
+                .filter(parkingLot -> parkingLot.getParkingRate() >= finalMostRate && parkingLot.getAvailbaleSpaces() > 0)
+                .max(Comparator.comparing(parkingLot -> parkingLot.getAvailbaleSpaces()))
                 .ifPresent(parkingLot -> ticket[0] = parkingLot.parking(car));
         return ticket[0];
 

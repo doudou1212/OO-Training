22faf837f86fba4ac180be9828f9414cb5d720b3
Jing Sun <jing.sun@rea-group.com>
2019-04-07 10:05:07 +0800

    Add test case for super parking boy: should park car to the parking lots which has the most rate

---
+++ src/test/SuperParkingBoyTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import main.homework.Car;
+import main.homework.ParkingBoy;
+import main.homework.ParkingLot;
+import main.homework.Ticket;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuperParkingBoyTest {
+    @Test
+    public void should_park_at_second_parking_lot_when_parking_given_1st_lot_rate_25_and_2nd_lot_rate_50() {
+        ParkingLot parkingLot1 = new ParkingLot(4);
+        parkingLot1.parking(new Car());
+        parkingLot1.parking(new Car());
+        parkingLot1.parking(new Car());
+
+        ParkingLot parkingLot2 = new ParkingLot(2);
+        parkingLot2.parking(new Car());
+
+        List<ParkingLot> parkingLots = new ArrayList<ParkingLot>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SuperParkingBoy superParkingBoy = new SuperParkingBoy(parkingLots);
+        Car car = new Car();
+        Ticket ticket = superParkingBoy.parking(car);
+        assertSame(car, parkingLot2.pick(ticket));
+
+    }
+}

7879a3cd8c1cca96e5d45bc4666b62ccabd8fdb3
Jing Sun <jing.sun@rea-group.com>
2019-04-07 16:04:45 +0800

    Add test case for manager: manger has two boys and only one boy has space, mange should parking success

--- src/test/ParkingManagerTest.java
+++ src/test/ParkingManagerTest.java
@@ -51,4 +51,32 @@ public class ParkingManagerTest {
         Ticket ticket = manager.parking(car);
         assertSame(car, manager.pick(ticket));
     }
+
+    @Test
+    public void should_get_parking_ticket_when_parking_given_parking_manager_has_two_boys_and_one_boy_has_one_available_parking_lot() {
+        ParkingLot boy1ParkingLot = new ParkingLot(1);
+        List<ParkingLot> boy1ParkingLots = new ArrayList<>();
+        boy1ParkingLots.add(boy1ParkingLot);
+        SmartParking smartParking = new SmartParking();
+        DefaultPick defaultPick = new DefaultPick();
+        ParkingBoy smartParkingBoy = new ParkingBoy(smartParking, defaultPick, boy1ParkingLots);
+        smartParkingBoy.parking(new Car());
+
+        ParkingLot boy2ParkingLot = new ParkingLot(1);
+        List<ParkingLot> boy2ParkingLots = new ArrayList<>();
+        boy2ParkingLots.add(boy2ParkingLot);
+        DefaultParking defaultParking = new DefaultParking();
+        ParkingBoy parkingBoy = new ParkingBoy(defaultParking, defaultPick, boy2ParkingLots);
+
+        List<ParkingBoy> boys = new ArrayList<>();
+        boys.add(smartParkingBoy);
+        boys.add(parkingBoy);
+
+        ManagerParking managerParking = new ManagerParking();
+        ManagePick managePick = new ManagePick();
+        ParkingManager manager = new ParkingManager(managerParking, managePick, boys);
+        Car car = new Car();
+        Ticket ticket = manager.parking(car);
+        assertSame(car, manager.pick(ticket));
+    }
 }
